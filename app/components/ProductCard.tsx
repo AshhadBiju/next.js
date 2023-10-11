d0f5eec36582256ad90d8d9bf46d74c080a9d207
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-10-11 15:51:36 +0530

    first-commit
    Mosh's Next.js part 1

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddToCart from './AddToCart';
+
+
+const ProductCard = () => {
+  return (
+    <div> 
+      <AddToCart />
+    </div>
+  );//after 'p-', enter Ctrl + Space to get the values
+}
+
+export default ProductCard;
