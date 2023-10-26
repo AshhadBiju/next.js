d1f91fd99154adbbf17469095ea34f9004232161
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-10-26 17:10:30 +0530

    update-done

--- app/components/updateagentdetails.js
+++ app/components/updateagentdetails.js
@@ -1,104 +1,133 @@
-'use client'
-import React, { useState } from 'react';
-import axios from 'axios'; // Import Axios
-import { useRouter } from 'next/navigation';
-import { toast, ToastContainer } from 'react-toastify';
+"use client";
+import React, { useState } from "react";
+import axios from "axios"; // Import Axios
+import { useRouter } from "next/navigation";
+import { toast, ToastContainer } from "react-toastify";
 
+export default function UpdateAgent({
+  id,
+  name,
+  password,
+  username,
+  email,
+  phonenumber,
+}) {
+  const [newUserName, setnewUserName] = useState(username);
+  const [newName, setnewName] = useState(name);
+  const [newEmail, setnewEmail] = useState(email);
+  const [newPassword, setnewPassword] = useState(password);
+  const [newPhoneNumber, setnewPhoneNumber] = useState(phonenumber);
+  const [newID, setId] = useState(id);
+  const router = useRouter(); //just before the handleSubmit where the agent updated response is stored
 
-const UpdateAgent = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    name: '',
-    email: '',
-    phoneNumber: '',
-  });
+  // const handleChange = (e) => {
+  //  const { name, value } = e.target;
+  // setFormData({ ...formData, [name]: value });
+  // };
 
-
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-  
-  const router = useRouter();//just before the handleSubmit where the agent updated response is stored
- 
-  const handleSubmit = (id) => {
-    id.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // Send a POST request to your API to Update the agent using Axios
-    axios.put(`http://localhost:3001/api/users/updateuser/${id}`, formData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        
-      })
-      .then((response) => {
-        console.log('Agent updated:', response.data);
-        toast.success('Agent has been updated');// You can handle success or display a success message here. import { toast, ToastContainer } from 'react-toastify' also. 
-      
-        router.push( "/agents");//this is the code to redirect to agents page. SET ALWAYS IN RESPONSE. Set a timeout to redirect as well. import this before handlesubmit const router = useRouter(); and this import { useRouter } from 'next/navigation';
-
-      })
-      .catch((error) => {
-        console.error('Error updating agent:', error);
-        // Handle the error and display an error message if needed
-      });
+    console.log(`id=${id}, ${newUserName}`);
+    try {
+      const res = axios
+        .put(
+          `http://localhost:3001/api/users/updateuser/${id}`,
+          {
+            name: newName,
+            password: newPassword,
+            username: newUserName,
+            email: newEmail,
+            phonenumber: newPhoneNumber,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            // body: JSON.stringify({
+            //   newUserName,
+            //   newName,
+            //   newEmail,
+            //   newPassword,
+            // }),
+          }
+        )
+        .then((result) => {
+          console.log(`responEditresult=${result.status}`);
+          console.log(`responEditresult=${result.data.name}`);
+        })
+        .catch((error) => {
+          console.log(`responEditerror=${error}`);
+        });
+      console.log(`responEdit=${res.status}`);
+      // if (!res.status.ok) {
+      //   console.log("Agent NOT updated:", res.data);
+      //   toast.failed("Agent has been updated"); // You can handle success or display a success message here. import { toast, ToastContainer } from 'react-toastify' also.
+      // }
+      router.push("/agents"); //this is the code to redirect to agents page. SET ALWAYS IN RESPONSE. Set a timeout to redirect as well. import this before handlesubmit const router = useRouter(); and this import { useRouter } from 'next/navigation';
+    } catch (error) {
+      console.error("Error updating agent:", error);
+      // Handle the error and display an error message if needed
+    }
   };
 
   return (
-    <form className='absolute right-20 top-20 shadow-2xl bg-sky-200' onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="absolute right-20 top-20 shadow-2xl bg-sky-200"
+    >
       <div>
-        <label>Username:</label>
+        <label>Name:</label>
         <input
+          onChange={(e) => setnewName(e.target.value)}
+          value={newName}
           type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
+          placeholder="name"
           required
         />
       </div>
       <div>
         <label>Password:</label>
         <input
+          onChange={(e) => setnewPassword(e.target.value)}
+          value={newPassword}
           type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
+          placeholder="Password"
           required
         />
       </div>
       <div>
-        <label>Name:</label>
+        <label>Username:</label>
         <input
+          onChange={(e) => setnewUserName(e.target.value)}
+          value={newUserName}
           type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
+          placeholder="Username"
+          required
         />
       </div>
       <div>
         <label>Email:</label>
         <input
+          onChange={(e) => setnewEmail(e.target.value)}
+          value={newEmail}
           type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
+          placeholder="Email"
           required
         />
       </div>
       <div>
-        <label>Phone Number:</label>
+        <label>Phone-number:</label>
         <input
+          onChange={(e) => setnewPhoneNumber(e.target.value)}
+          value={newPhoneNumber}
           type="text"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-        required
+          placeholder="Phone-number"
         />
       </div>
-      <button className='bg-sky-700' type="submit" >Update Agent</button>
+      <button className="bg-sky-700" type="submit">
+        Update Agent
+      </button>
     </form>
   );
-};
-
-export default UpdateAgent;
+}
--- app/updateagent/[id]/page.jsx
+++ app/updateagent/[id]/page.jsx
@@ -1,34 +1,56 @@
-import UpdateAgent from '@/app/components/updateagentdetails'
+import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import UpdateAgent from "@/app/components/updateagentdetails";
 
-const getAgentById = async (id) => {
-    try {
-        const res = await fetch(`http://localhost:3001/api/users/updateuser/${id}`, formData, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Cache-Control': 'no-store',
-            },
-            
-          })
-          .if(!res.ok) {
-            throw new Error("   Failed to fetch Agent");
-          }
-
-return res.json ();
-        } catch (error) {
-           
-            console.log("catch error");
-           
-        }}
-    };
-
-
-
-export default function EditAgents({ params }) {
-    const { id } = params;
-    await 
-    console.log("id:",id);
-    return (
-      <div>
-    <UpdateAgent/>  </div>
-    );
+export default async function EditAgents({ params }) {
+  const { id } = params;
+  console.log(`idid=${id}`);
+  const agentData = await getAgentById(id);
+  console.log(`agentData=${agentData}`);
+  if (!agentData) {
+    return null; // Render loading or error state while fetching data
   }
+
+  return <UpdateAgent {...agentData} />;
+}
+const getAgentById = async (idn) => {
+  try {
+    console.log(`idid=${idn}`);
+    const url = `http://localhost:3001/api/users/getuser/${idn}`;
+    const res = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
+      },
+    });
+    console.log(`res=${res}`);
+
+    if (!res.data) {
+      throw new Error("Failed to fetch Agent");
+    }
+
+    const agent = res.data;
+
+    if (!agent) {
+      console.error("Agent data is undefined");
+      // Handle the error appropriately, e.g., display an error message to the user.
+    }
+
+    const { id, name, password, username, email, phonenumber } = agent;
+    console.log(`name=${id}`);
+
+    return {
+      id,
+      name,
+      password,
+      username,
+      email,
+      phonenumber,
+    };
+  } catch (error) {
+    console.error("Error fetching agent data:", error);
+    // Handle the error appropriately, e.g., display an error message to the user.
+    return null;
+  }
+};
